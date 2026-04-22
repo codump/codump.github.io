33e04abdd4f8c5b64fef1ef1594b91308934e8ae
kip <info@brotherops.com>
2026-04-22 10:29:30 +0000

    loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(window).on('load', function () {
-  if (!sessionStorage.getItem('loaderShown')) {
+  if (sessionStorage.getItem('loaderShown')) {
+    $('#load').hide();
+  } else {
     $('#loadtxt1')
       .removeClass('loadOK')
       .delay(1500)
@@ -30,7 +32,5 @@ $(window).on('load', function () {
     });
 
     sessionStorage.setItem('loaderShown', 'true');
-  } else {
-    $('#load').hide();
   }
 });
