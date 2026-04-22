30acb7b2698f3fa39f6e65093d14f5b0c5ba7935
kip <info@brotherops.com>
2026-04-22 10:23:52 +0000

    loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,32 @@
-//alert('ok')
 $(window).on('load', function () {
-	// loader //
-	$("#loadtxt1").removeClass("loadOK").delay(1500).queue(function(next) {
-		$(this).addClass("loadOK").delay(500).queue(function(next2) {
-			$("#loadtxt2 span").css('color','#00b646');
-			$("#loadtxt1").animate({
-				marginLeft: '7.8rem'
-			}, 500);
-			$("#loadtxt2").animate({
-				marginLeft: '15.8rem'
-			}, 500);
-			next2();
-		});
-		next();
-	});
-  $("#load").ready(function() {
-		$("#progress").animate({
-			width: '20%'
-		}, 500);
-		$("#progress2").animate({
-			width: '20%'
-		}, 500);
-	});
-	$("#sidebar").ready(function() {
-		$("#progress").animate({
-			width: '75%'
-		}, 500);
-		$("#progress2").animate({
-			width: '75%'
-		}, 500);
-	});
-	$("#main-wrapper").ready(function() {
-		$("#progress").animate({
-			width: '100%'
-		}, 500);
-		$("#progress2").animate({
-			width: '100%'
-		}, 500);
-		$("#load").delay(3000).fadeOut(200);
-	});
-	// loader //
+    if (!sessionStorage.getItem('loaderShown')) {
+
+        $("#loadtxt1").removeClass("loadOK").delay(1500).queue(function(next) {
+            $(this).addClass("loadOK").delay(500).queue(function(next2) {
+                $("#loadtxt2 span").css('color','#00b646');
+                $("#loadtxt1").animate({ marginLeft: '7.8rem' }, 500);
+                $("#loadtxt2").animate({ marginLeft: '15.8rem' }, 500);
+                next2();
+            });
+            next();
+        });
+
+        $("#load").ready(function() {
+            $("#progress, #progress2").animate({ width: '20%' }, 500);
+        });
+
+        $("#sidebar").ready(function() {
+            $("#progress, #progress2").animate({ width: '75%' }, 500);
+        });
+
+        $("#main-wrapper").ready(function() {
+            $("#progress, #progress2").animate({ width: '100%' }, 500);
+            $("#load").delay(3000).fadeOut(200);
+        });
+
+        sessionStorage.setItem('loaderShown', 'true');
+
+    } else {
+        $("#load").hide();
+    }
 });
